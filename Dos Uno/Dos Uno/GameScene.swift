fd23396415216e8e4e763c846aa9a31969dd12b0
Evan McNulty <muchmusic@me.com>
2016-02-09 20:41:31 -0800

    Added a test asset and a sprite which is displayed

--- Dos Uno/Dos Uno/GameScene.swift
+++ Dos Uno/Dos Uno/GameScene.swift
@@ -11,20 +11,22 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        let redZeroSprite = SKSpriteNode(imageNamed:"Red_0")
         
-        self.addChild(myLabel)
+        redZeroSprite.xScale = 0.6
+        redZeroSprite.yScale = 0.6
+        redZeroSprite.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        self.addChild(redZeroSprite)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
+        // default template sprite action
+        /*
         for touch in touches {
             let location = touch.locationInNode(self)
-            
+        
             let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
             sprite.xScale = 0.5
@@ -34,9 +36,11 @@ class GameScene: SKScene {
             let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
             sprite.runAction(SKAction.repeatActionForever(action))
+
             
             self.addChild(sprite)
         }
+        */
     }
    
     override func update(currentTime: CFTimeInterval) {
